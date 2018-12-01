89d43032d96d42c644df1bc2bf5d38000ea476cb
Jose Gonzalez <jgpereira23@gmail.com>
2018-11-30 20:22:19 -0500

    Sec 80: Working with the FIle URL

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -24,7 +24,8 @@ export class PostsService {
           return {
             title: post.title,
             content: post.content,
-            id: post._id
+            id: post._id,
+            imagePath: post.imagePath
           };
         });
       }))
@@ -43,12 +44,13 @@ export class PostsService {
     postData.append('title', title);
     postData.append('content', content);
     postData.append('image', image, title);
-    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', postData)
+    this.http.post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData)
       .subscribe((responseData) => {
         const post: Post = {
-          id: responseData.postId,
-          title: title,
-          content: content
+          id: responseData.post.id,
+          title: responseData.post.title,
+          content: responseData.post.content,
+          imagePath: responseData.post.imagePath
         };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
@@ -66,7 +68,7 @@ export class PostsService {
   }
 
   updatePost(postId: string, title: string, content: string) {
-    const post: Post = {id: postId, title: title, content: content};
+    const post: Post = {id: postId, title: title, content: content, imagePath: null};
     this.http.put('http://localhost:3000/api/posts/' + postId, post)
       .subscribe(response => {
         const updatedPosts = [...this.posts];
