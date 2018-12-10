7fdc86bb771f09dc59d42672c495aae2b2688c1f
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-09 21:45:38 -0500

    Sec 99: Connecting Angular to the Backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,26 +5,26 @@ const User = require("../models/user");
 
 const router = express.Router();
 
-router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-  .then( hash => {
-    const user = new User ({
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
       email: req.body.email,
-      password:has
+      password: hash
     });
-  });
-  user.save()
-  then(result => {
-    res.status(201).json({
-      message: 'User created!',
-      result: result
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
       });
-    });
-  })
+  });
 });
 
 module.exports = router;
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -7,8 +8,11 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent {
   isLoading = false;
-
-  signUp(form: NgForm) {
-    console.log(form.value);
+  constructor(public authService: AuthService) {}
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
