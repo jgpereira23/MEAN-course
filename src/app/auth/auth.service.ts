9a673b5b11fea434db875acad0da0dbe4a0b1649
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-11 22:58:49 -0500

    Sec 104: Adding the Toekn to Authenticate Requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,12 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private token: string;
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = {
@@ -24,9 +29,10 @@ export class AuthService {
       password: password
     };
     this.http
-      .post('http://localhost:3000/api/user/login', authData)
+      .post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 }
